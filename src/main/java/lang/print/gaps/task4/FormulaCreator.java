0464d3f03701e772eb9f04b3b107cf4b14492654
victor diaz <vdmpeniel@yahoo.es>
2023-06-11 16:32:59 -0400

    Added changes for all the subtasks.

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a  = 1, b = 1;
+        double c;
+        c = (9 * (a ^ 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -1,6 +1,16 @@
 package lang.print.gaps.task4;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class IntRepresentation {
     public static void main(String[] args) {
+        //Arrays.asList('c', 'h', 'a', 'r').forEach(c -> System.out.println((int)c));
+
+        System.out.println(99);
+        System.out.println(104);
+        System.out.println(97);
+        System.out.println(114);
+
     }
 }
